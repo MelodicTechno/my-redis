c68e4b616b6c68c05631170d59b94c8270ca27dd
MelodicTechno <617179025@qq.com>
2025-02-16 11:04:27 +0800

    to message

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,34 +1,47 @@
+use std::collections::HashMap;
+
+use bytes::Bytes;
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use std::sync::{Mutex, Arc};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+type SharedDb = Arc<Vec<Mutex<HashMap<String, Vec<u8>>>>>;
 
 #[tokio::main]
 async fn main() {
     // bind
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
+
+        println!("Accepted!");
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    let mut db = HashMap::new();
 
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     Frame::Bulk(value.clone().into())
                 } else {
@@ -40,4 +53,13 @@ async fn process(socket: TcpStream) {
 
         connection.write_frame(&response).await.unwrap();
     }
+}
+
+fn new_shared_db(num_shards: usize) -> SharedDb {
+    let mut db = Vec::with_capacity(num_shards);
+    for _ in 0..num_shards {
+        db.push(Mutex::new(HashMap::new()));
+    }
+
+    Arc::new(db)
 }
